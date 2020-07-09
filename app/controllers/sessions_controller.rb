43635d7f08e368f51b3cd2b65ad76c64347f1807
KessaPassa <k.someya.gyao@gmail.com>
2020-07-09 22:12:56 +0900

    [bugfix] users_pathをuser_pathに変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
+      remember user
       log_in user
-      redirect_to users_path(user)
+      redirect_to user
     else
       flash.now[:danger] = 'invalid email/password combination'
       render 'new'
